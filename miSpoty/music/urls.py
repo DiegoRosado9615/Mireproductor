32eb396d6a9b9decebb852f8bfb3de91c933b384
LoboStark <rocadie9615@ciencias.unam.mx>
2020-03-11 16:51:45 -0600

    Por si no acabo :,V

--- miSpoty/music/urls.py
+++ miSpoty/music/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index, name = 'index'),
-    path('cancionesPopulares/', views.cancionesPopulares,name="cancionesPopulares")
+    path('', views.home, name="home"),
+    path('cancionesPopulares/', views.cancionesPopulares,name="cancionesPopulares"),
+    path('player/', views.playerMusic,name="reproductorMusical")
+
 ]
--- miSpoty/music/views.py
+++ miSpoty/music/views.py
@@ -5,6 +5,14 @@ from django.template import loader
 def index (request):
     return HttpResponse("Bienvenidos")
 
+def home(request):
+    context={}
+    return render(request,'music/home.html',context)
+
 def cancionesPopulares(request):
     context={}
     return render(request,'music/index.html',context)
+
+def playerMusic(request):
+    context={}
+    return render(request,'music/player.html',context)
